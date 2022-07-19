104f8f34bf8cea71dcb688ca2ea6ed879e900f9e
Marvin Njuguna <marvinsinbox@gmail.com>
2022-07-19 12:29:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+random_chars = "abcdefghiklmnopqrstvxyzABCDEFGHIKLMNOPQRSTVXYZ0123456789!@#$%"
+
+while 1:
+  password_len = int(input("How long would you like your password: "))
+  password_count = int(input("How many passwords would you like: "))
+  for x in range(0, password_count):
+    password = ""
+    for x in range(0, password_len):
+      password_char = random.choice(random_chars)
+      password = password + password_char
+    print("Your password is: {}".format(password))
